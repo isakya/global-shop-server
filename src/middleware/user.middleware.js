92afc06df7329b44d4d8c6ae807aaa190e6011e2
konglong <1016361268@qq.com>
2022-06-11 22:14:52 +0800

    验证登陆

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,7 +1,7 @@
 // 导入密码加密包
 const bcrypt = require('bcryptjs')
 const { getUserInfo } = require('../service/user.service')
-const { userFormateError, userAlreadyExited, userRegisterError } = require('../constant/err.type')
+const { userFormateError, userAlreadyExited, userRegisterError, userDoesNotExit, userLoginError, invalidPassword } = require('../constant/err.type')
 
 
 const userValidator = async (ctx, next) => {
@@ -50,7 +50,28 @@ const cryptPassword = async (ctx, next) => {
 
 const verifyLogin = async (ctx, next) => {
   // 1. 判断用户是否存在(不存在报错)
-  // 2. 密码是否匹配(不匹配: 报错)
+  const { user_name, password } = ctx.request.body
+
+  // 注意：try..catch 有块级作用域
+  try {
+    const res = await getUserInfo({ user_name })
+    if (!res) {
+      console.error('用户不存在', { user_name })
+      ctx.app.emit('error', userDoesNotExit, ctx)
+      return
+    }
+    // 2. 密码是否匹配(不匹配: 报错)
+    if (!bcrypt.compareSync(password, res.password)) {
+      ctx.app.emit('error', invalidPassword, ctx)
+      return
+    }
+  } catch (err) {
+    console.error(err)
+    return ctx.app.emit('error', userLoginError, ctx)
+  }
+
+
+
 
 
   await next()
