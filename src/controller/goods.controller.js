223011fc92eb6322a6f9196b3bd84940d485ae44
konglong <1016361268@qq.com>
2022-06-13 01:27:38 +0800

    检查文件类型是否符合要求

--- src/controller/goods.controller.js
+++ src/controller/goods.controller.js
@@ -1,11 +1,15 @@
 const path = require('path')
-const { fileUploadError } = require('../constant/err.type')
+const { fileUploadError, unSupportedFileType } = require('../constant/err.type')
 class GoodsController {
   async upload(ctx, next) {
     // console.log(ctx.request.files.file)
     // file 是自定义名称，前端传过来的
     const { file } = ctx.request.files
+    const fileTypes = ['image/jpeg', 'image/png']
     if (file) {
+      if (!fileTypes.includes(file.type)) {
+        return ctx.app.emit('error', unSupportedFileType, ctx)
+      }
       ctx.body = {
         code: 0,
         message: '图片上传成功',
