fe728f8ac4a5f7aab1d0f7f33bc89602f306eaba
konglong <1016361268@qq.com>
2022-06-14 00:58:24 +0800

    打通添加到购物车方法的流程

--- src/router/cart.route.js
+++ src/router/cart.route.js
@@ -4,7 +4,9 @@ const Router = require('koa-router')
 // 中间件
 const { auth } = require('../middleware/auth.middleware')
 const { validator } = require('../middleware/cart.middleware')
+
 // 控制器
+const { add } = require('../controller/cart.controller')
 
 // 2. 实例化router对象
 const router = new Router({ prefix: '/carts' })
@@ -12,9 +14,7 @@ const router = new Router({ prefix: '/carts' })
 // 3. 编写路由规则
 
 // 3.1 添加到购物车接口：登陆，格式
-router.post('/', auth, validator, (ctx) => {
-  ctx.body = '添加成功'
-})
+router.post('/', auth, validator, add)
 
 // 4. 导出router对象 
 module.exports = router
